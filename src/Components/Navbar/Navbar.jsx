372ca7b935ac90055651ffa8a79ad9bc9972841e
sudiptodas153 <sudiptodas4093@gmail.com>
2025-05-24 17:31:00 +0600

    add shadow in the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
 
     return (
         <div className=''>
-            <div className="navbar bg-base-100 ">
+            <div className="navbar bg-base-100  shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
